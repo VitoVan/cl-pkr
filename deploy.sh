690360d6ca0bedf4d01f2086a9a98c86c1065a31
Vito Van <vito@sdf.org>
2022-09-29 20:26:17 +0800

    fix: fix mac starter

--- deploy.sh
+++ deploy.sh
@@ -89,9 +89,8 @@ then
     # copy libzstd
     cp /usr/local/lib/libzstd.1.dylib $OSX_APP_DIR/MacOS/
     # set DYLD fallback
-    echo "#!/bin/bash" >> $OSX_APP_DIR/MacOS/color-picker
-    echo "DYLD_FALLBACK_LIBRARY_PATH=./ ./color-picker-bin" >> $OSX_APP_DIR/MacOS/color-picker
-    chmod +x $OSX_APP_DIR/MacOS/color-picker
+    cp ./resources/color-picker.sh $OSX_APP_DIR/color-picker
+    chmod +x $OSX_APP_DIR/MacOS/*
     mkdir -p $OSX_APP_DIR/Resources
     cp ./resources/iconfile.icns $OSX_APP_DIR/Resources
     cd out && zip -r -9 color-picker$APP.zip color-picker.app && cd ..
